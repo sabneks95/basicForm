7df05eb85035ef1723ba0fbf3a018a3fbf6d3007
sabneks95 <95006362+sabneks95@users.noreply.github.com>
2022-11-08 20:35:12 +0530

    Template Driven Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basicForm';
+userdata:any='';
+  getData(data:NgForm){
+    console.log(data);
+    this.userdata=data;
+  }
 }
